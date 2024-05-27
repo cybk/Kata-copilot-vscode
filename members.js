7d6ff9e5f5879b6a7a07120cf04b7a2e9d9d028b
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2024-05-27 01:23:20 +0000

    Adding second test file for copilot kata

---
+++ members.js
@@ -0,0 +1,9 @@
+function skillMembers() {
+    var members = [
+        { name: "John", skill: "HTML" },
+        { name: "Jane", skill: "CSS" },
+        { name: "Jack", skill: "JavaScript" }
+    ];
+
+    return members;
+}
